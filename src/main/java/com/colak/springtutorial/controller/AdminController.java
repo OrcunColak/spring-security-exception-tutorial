759662c8bc90ee90ab8d8d78ac24aec64812c7a6
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-08 13:23:43 +0300

    method security example

--- src/main/java/com/colak/springtutorial/controller/AdminController.java
+++ src/main/java/com/colak/springtutorial/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.colak.springtutorial.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "adminController")
 @RequestMapping("/api/v1")
 public class AdminController {
 
@@ -20,8 +23,21 @@ public class AdminController {
     // http://localhost:8080/api/v1/secured/admin/adminstrator
     @GetMapping(value = "/secured/admin/{userId}")
     @PreAuthorize("hasRole('ROLE_ADMIN') and @mySecurityService.canEditUser(#userId)")
-    public String securedForAdminAndUserCall(@PathVariable String userId) {
+    public String securedForAdminCall(@PathVariable String userId) {
         return "hello world secured!! for " + userId;
+    }
+
+    // Spring 3.3 style
+    // http://localhost:8080/api/v1/secured/admin2/administrator
+    @GetMapping(value = "/secured/admin2/{userId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') and (#userId == @adminController.getCurrentUser())")
+    public String securedForAdminCall2(@PathVariable String userId) {
+        return "hello world secured!! for " + userId;
+    }
+
+    public String getCurrentUser() {
+        UserDetails currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return currentUser.getUsername();
     }
 
 }
